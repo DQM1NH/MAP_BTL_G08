0fe44b359718a0c71c29ab04ae53d595b167040e
DQM1NH <mdang9531@gmail.com>
2025-11-02 09:48:38 +0700

    update_02_11

--- app/src/main/java/com/example/map_btl_g08/Privacy_policy.java
+++ app/src/main/java/com/example/map_btl_g08/Privacy_policy.java
@@ -3,6 +3,7 @@ package com.example.map_btl_g08;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +13,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Privacy_policy extends AppCompatActivity {
     ImageView btnClose;
+    TextView tvPrivacy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+        // anh xa id
         btnClose = findViewById(R.id.btnClose);
+        tvPrivacy = findViewById(R.id.tvPrivacy_first);
+        tvPrivacy.setText(getString(R.string.text_privacy));
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
