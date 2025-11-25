9d1d675eec7c3eacfbb51b1b171cbb9a86985f28
DQM1NH <mdang9531@gmail.com>
2025-11-25 20:56:25 +0700

    update_25_11

---
+++ app/src/main/java/com/example/map_btl_g08/Splash_Screen_Activity.java
@@ -0,0 +1,31 @@
+package com.example.map_btl_g08;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash_Screen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash_Screen_Activity.this, Start_Game.class);
+            startActivity(intent);
+            finish();   // đóng splash screen
+        }, 4000);
+    }
+}
